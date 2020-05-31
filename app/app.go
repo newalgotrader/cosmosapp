781684b412cd53531896bfc96fad8c3d301b7b13
Nick DeLuca <nick@newalgotrader.com>
2020-05-31 16:22:40 -0500

    fill out app file and tests with basics - start integrating and building out daemon and cli next

--- app/app.go
+++ app/app.go
@@ -1,1 +1,42 @@
 package app
+
+import (
+	"github.com/tendermint/tendermint/libs/log"
+	dbm "github.com/tendermint/tm-db"
+
+	bam "github.com/cosmos/cosmos-sdk/baseapp"
+	"github.com/cosmos/cosmos-sdk/codec"
+	"github.com/cosmos/cosmos-sdk/x/auth"
+)
+
+// passed to baseapp, returned by app.Name()
+const appName = "app"
+
+// cosmos app built on baseapp
+type App struct {
+	*bam.BaseApp
+}
+
+// return new app
+func NewApp(logger log.Logger, db dbm.DB) *App {
+	cdc := MakeCodec()
+	txDecoder := auth.DefaultTxDecoder(cdc)
+
+	bApp := bam.NewBaseApp(appName, logger, db, txDecoder)
+
+	app := &App{BaseApp: bApp}
+
+	return app
+}
+
+// create a codec will all required registrations for the application
+func MakeCodec() *codec.Codec {
+	var cdc = codec.New()
+
+	//ModuleBasics.RegisterCodec(cdc)
+	//vesting.RegisterCodec(cdc)
+	//sdk.RegisterCodec(cdc)
+	//codec.RegisterCrypto(cdc)
+	return cdc.Seal()
+
+}
--- app/app_test.go
+++ app/app_test.go
@@ -4,8 +4,24 @@ import (
 	"testing"
 
 	"github.com/stretchr/testify/require"
+	"github.com/tendermint/tendermint/libs/log"
+	dbm "github.com/tendermint/tm-db"
 )
 
+func TestMakeCodec(t *testing.T) {
+	cdc := MakeCodec()
+
+	require.Panics(t,
+		func() { cdc.RegisterInterface((*interface{})(nil), nil) },
+		"panics when new registrations are attempted (cdc is sealed)")
+}
+
 func TestApp(t *testing.T) {
-	require.Equal(t, "Placeholder", "Placeholder")
+	logger := log.NewNopLogger()
+	db := dbm.NewMemDB()
+
+	app := NewApp(logger, db)
+
+	require.Equal(t, app.Name(), "app", "name is set to 'app'")
+	//require.Equal(t, app.AppVersion(), "0.0.1", "version is 0.0.1")
 }
